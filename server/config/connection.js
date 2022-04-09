f3a495a27476246955af96d884003d1e829c99e7
mflanigan <mflanigantwualumn@gmail.com>
2022-04-09 15:08:53 -0500

    finished module 21

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/deep-thoughts',
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
